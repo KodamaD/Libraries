837b14b3c6624a457a9228adea7bc6054273745c
KoD <20050904daikikodama@gmail.com>
2020-02-21 23:02:30 +0900

    modify template

--- template/fix_point.cpp
+++ template/fix_point.cpp
@@ -6,7 +6,7 @@ struct fix_point: private T {
   explicit constexpr fix_point(T &&func): T(std::forward<T>(func)) { }
   template <class... Args>
   constexpr decltype(auto) operator () (Args &&... args) const {
-    return T::operator()(*this, std::forward<Args>(args...)...);
+    return T::operator()(*this, std::forward<Args>(args)...);
   }
 };
 
