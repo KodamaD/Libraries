4f3b6aceefc9384dcba45b51d3a7b6e1540517d7
KoD <20050904daikikodama@gmail.com>
2020-03-14 09:37:39 +0900

    added rbst

---
+++ query/advanced_array.cpp
@@ -0,0 +1,187 @@
+
+#include <ctime>
+#include <utility>
+#include <vector>
+
+template <class T>
+class advanced_array {
+public:
+  struct node_type;
+  using root_type = node_type *;
+  using value_type = T;
+  using size_type = unsigned;
+  using flag_type = bool;
+
+  struct node_type {
+    root_type left, right;
+    value_type value;
+    size_type size;
+    flag_type reversed;
+  };
+
+private:
+  root_type M_root;
+
+  inline size_type engine() const {
+    static size_type current = std::clock() + std::time(nullptr);
+    current ^= (current << 13);
+    current ^= (current >> 17);
+    current ^= (current << 5);
+    return current;
+  }
+
+  inline size_type size(root_type node) const {
+    return node ? node -> size : 0;
+  }
+  inline root_type apply(root_type node) const {
+    node -> size = size(node -> left) + size(node -> right) + 1;
+    return node;
+  }
+  inline void flush(root_type node) const {
+    if (node -> reversed) {
+      std::swap(node -> left, node -> right);
+      if (node -> left) node -> left -> reversed ^= 1;
+      if (node -> right) node -> right -> reversed ^= 1;
+      node -> reversed = false;
+    }
+  }
+
+  root_type M_merge_impl(root_type left, root_type right) const {
+    if (!left || !right) return left ? left : right;
+    if (engine() % (size(left) + size(right)) < size(left)) {
+      flush(left);
+      left -> right = M_merge_impl(left -> right, right);
+      return apply(left);
+    }
+    else {
+      flush(right);
+      right -> left = M_merge_impl(left, right -> left);
+      return apply(right);
+    }
+  }
+  std::pair<root_type, root_type> M_split_impl(root_type node, size_type pos) const {
+    if (!node) return { nullptr, nullptr };
+    flush(node);
+    if (pos <= size(node -> left)) {
+      auto tmp = M_split_impl(node -> left, pos);
+      node -> left = tmp.second;
+      return { tmp.first, apply(node) };
+    }
+    else {
+      auto tmp = M_split_impl(node -> right, pos - size(node -> left) - 1);
+      node -> right = tmp.first;
+      return { apply(node), tmp.second };
+    }
+  }
+
+  root_type M_build_impl(const std::vector<value_type> &data, size_type l, size_type r) const {
+    if (l >= r) return nullptr;
+    size_type idx = engine() % (r - l) + l;
+    return apply(new node_type{ M_build_impl(data, l, idx), M_build_impl(data, idx + 1, r), data[idx], 1 });
+  }
+  void M_clear_impl(root_type node) const {
+    if (!node) return;
+    M_clear_impl(node -> left);
+    M_clear_impl(node -> right);
+    delete node;
+  }
+  void M_dump_impl(root_type node, std::vector<value_type> &data) const {
+    if (!node) return;
+    flush(node);
+    M_dump_impl(node -> left, data);
+    data.push_back(node -> value);
+    M_dump_impl(node -> right, data);
+  }
+
+  root_type M_insert_single_impl(root_type node, size_type idx, const value_type &val) const {
+    auto tmp = M_split_impl(node, idx);
+    return M_merge_impl(M_merge_impl(tmp.first, new node_type{ nullptr, nullptr, val, 1 }), tmp.second);
+  }
+  root_type M_insert_range_impl(root_type node, size_type idx, const std::vector<value_type> &data) const {
+    auto tmp = M_split_impl(node, idx);
+    return M_merge_impl(M_merge_impl(tmp.first, M_build_impl(data, 0, data.size())), tmp.second);
+  }
+  root_type M_erase_single_impl(root_type node, size_type idx) const {
+    auto left = M_split_impl(node, idx);
+    auto right = M_split_impl(left.second, 1);
+    delete right.first;
+    return M_merge_impl(left.first, right.second);
+  }
+  root_type M_erase_range_impl(root_type node, size_type l, size_type r) const {
+    auto left = M_split_impl(node, l);
+    auto right = M_split_impl(left.second, r - l);
+    M_clear_impl(right.first);
+    return M_merge_impl(left.first, right.second);
+  }
+
+public:
+  advanced_array(): M_root(nullptr) { }
+  advanced_array(const std::vector<value_type> &data): M_root(M_build_impl(data, 0, data.size())) { }
+  ~advanced_array() { M_clear_impl(M_root); }
+
+  void insert_at(size_type idx, const value_type &val) {
+    M_root = M_insert_single_impl(M_root, idx, val);
+  }
+  void insert_range(size_type idx, const std::vector<value_type> &data) {
+    M_root = M_insert_range_impl(M_root, idx, data);
+  }
+  void erase_at(size_type idx) {
+    M_root = M_erase_single_impl(M_root, idx);
+  }
+  void erase_range(size_type l, size_type r) {
+    M_root = M_erase_range_impl(M_root, l, r);
+  }
+
+  void push_front(const value_type &val) {
+    M_root = M_merge_impl(new node_type{ nullptr, nullptr, val, 1 }, M_root);
+  }
+  void push_back(const value_type &val) {
+    M_root = M_merge_impl(M_root, new node_type{ nullptr, nullptr, val, 1 });
+  }
+  void pop_front() {
+    M_root = M_erase_single_impl(M_root, 0);
+  }
+  void pop_back() {
+    M_root = M_erase_single_impl(M_root, size(M_root) - 1);
+  }
+
+  void reverse(size_type l, size_type r) {
+    auto left = M_split_impl(M_root, l);
+    auto right = M_split_impl(left.second, r - l);
+    right.first -> reversed ^= 1;
+    M_root = M_merge_impl(left.first, M_merge_impl(right.first, right.second));
+  }
+  void clear() {
+    M_clear_impl(M_root);
+  }
+  flag_type empty() const {
+    return size(M_root) == 0;
+  }
+  size_type size() const {
+    return size(M_root);
+  }
+
+  value_type &operator [] (size_type idx) {
+    root_type cur = M_root;
+    size_type cnt = 0;
+    while (true) {
+      flush(cur);
+      if (cnt + size(cur -> left) == idx) return cur -> value;
+      else if (cnt + size(cur -> left) > idx) cur = cur -> left;
+      else cnt += size(cur -> left) + 1, cur = cur -> right;
+    }
+  }
+  std::vector<value_type> dump(size_type l, size_type r) {
+    auto left = M_split_impl(M_root, l);
+    auto right = M_split_impl(left.second, r - l);
+    std::vector<value_type> res;
+    M_dump_impl(right.first, res);
+    M_root = M_merge_impl(left.first, M_merge_impl(right.first, right.second));
+    return res;
+  }
+
+};
+
+int main() {
+  return 0;
+}
--- query/avl_tree.cpp
+++ query/avl_tree.cpp
@@ -5,48 +5,48 @@ template <class T>
 class avl_tree {
 public:
   struct node_type;
-  using node_ptr = node_type *;
+  using root_type = node_type *;
   using value_type = T;
   using size_type = int;
+  using flag_type = bool;
 
   struct node_type {
-    node_ptr left, right;
+    root_type left, right;
     value_type value;
     size_type size, height;
   };
 
 private:
-  node_ptr M_root;
-  size_type M_node_count;
+  root_type M_root;
 
-  inline size_type size(node_ptr node) const {
+  inline size_type size(root_type node) const {
     return node ? node -> size : 0;
   }
-  inline size_type height(node_ptr node) const {
+  inline size_type height(root_type node) const {
     return node ? node -> height : 0;
   }
-  inline size_type coeff(node_ptr node) const {
+  inline size_type coeff(root_type node) const {
     return node ? height(node -> left) - height(node -> right) : 0;
   }
 
-  inline node_ptr apply(node_ptr node) {
+  inline root_type apply(root_type node) {
     node -> size = size(node -> left) + size(node -> right) + 1;
     node -> height = std::max(height(node -> left), height(node -> right)) + 1;
     return node;
   }
-  inline node_ptr rotate_l(node_ptr node) {
-    node_ptr new_root = node -> right;
+  inline root_type rotate_l(root_type node) {
+    root_type new_root = node -> right;
     node -> right = new_root -> left;
     new_root -> left = node;
     return apply(node), apply(new_root);
   }
-  inline node_ptr rotate_r(node_ptr node) {
-    node_ptr new_root = node -> left;
+  inline root_type rotate_r(root_type node) {
+    root_type new_root = node -> left;
     node -> left = new_root -> right;
     new_root -> right = node;
     return apply(node), apply(new_root);
   }
-  inline node_ptr balance(node_ptr node) {
+  inline root_type balance(root_type node) {
     size_type dif = coeff(apply(node));
     if (dif == 2) {
       if (coeff(node -> left) < 0) node -> left = rotate_l(node -> left);
@@ -59,31 +59,30 @@ private:
     return node;
   }
 
-  node_ptr M_insert_impl(node_ptr node, const value_type &val) {
+  root_type M_insert_impl(root_type node, const value_type &val) {
     if (!node) return new node_type{ nullptr, nullptr, val, 1, 1 };
     if (val < node -> value) node -> left = M_insert_impl(node -> left, val);
     else node -> right = M_insert_impl(node -> right, val);
     return balance(node);
   }
-  node_ptr M_erase_impl(node_ptr node, const value_type &val) {
+  root_type M_erase_impl(root_type node, const value_type &val) {
     if (!node) return nullptr;
     if (val < node -> value) node -> left = M_erase_impl(node -> left, val);
     else if (val > node -> value) node -> right = M_erase_impl(node -> right, val);
     else {
       if (!(node -> left) || !(node -> right)) {
-        --M_node_count;
-        node_ptr res = node -> left ? node -> left : node -> right;
+        root_type res = node -> left ? node -> left : node -> right;
         delete node;
         return res;
       }
-      node_ptr right_end = node -> left;
+      root_type right_end = node -> left;
       while (right_end -> right) right_end = right_end -> right;
       node -> value = right_end -> value;
       node -> left = M_erase_impl(node -> left, right_end -> value);
     }
     return balance(node);
   }
-  void M_clear_impl(node_ptr node) {
+  void M_clear_impl(root_type node) {
     if (!node) return;
     M_clear_impl(node -> left);
     M_clear_impl(node -> right);
@@ -91,17 +90,11 @@ private:
   }
 
 public:
-  avl_tree():
-    M_root(nullptr),
-    M_node_count(0)
-  { }
-  ~avl_tree() {
-    M_clear_impl(M_root);
-  }
+  avl_tree(): M_root(nullptr) { }
+  ~avl_tree() { M_clear_impl(M_root); }
 
   void insert(const value_type &val) {
     M_root = M_insert_impl(M_root, val);
-    ++M_node_count;
   }
   void erase(const value_type &val) {
     M_root = M_erase_impl(M_root, val);
@@ -110,11 +103,11 @@ public:
     M_clear_impl(M_root);
   }
 
-  bool empty() const {
-    return M_node_count == 0;
+  flag_type empty() const {
+    return size(M_root) == 0;
   }
-  bool contains(const value_type &val) const {
-    node_ptr cur = M_root;
+  flag_type contains(const value_type &val) const {
+    root_type cur = M_root;
     while (cur) {
       if (val == cur -> value) return true;
       if (val < cur -> value) cur = cur -> left;
@@ -124,10 +117,10 @@ public:
   }
 
   size_type size() const {
-    return M_node_count;
+    return size(M_root);
   }
   size_type lower_bound(const value_type &val) const {
-    node_ptr cur = M_root;
+    root_type cur = M_root;
     size_type cnt = 0;
     while (cur) {
       if (val <= cur -> value) cur = cur -> left;
@@ -136,7 +129,7 @@ public:
     return cnt;
   }
   size_type upper_bound(const value_type &val) const {
-    node_ptr cur = M_root;
+    root_type cur = M_root;
     size_type cnt = 0;
     while (cur) {
       if (val < cur -> value) cur = cur -> left;
@@ -146,7 +139,7 @@ public:
   }
 
   value_type at(size_type idx) const {
-    node_ptr cur = M_root;
+    root_type cur = M_root;
     size_type cnt = 0;
     while (true) {
       if (cnt + size(cur -> left) == idx) return cur -> value;
@@ -156,7 +149,6 @@ public:
   }
 
 };
-
 
 int main() {
   return 0;
