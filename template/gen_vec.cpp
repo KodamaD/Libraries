1e7fcd868293cf86ea1a7cea04f2bcccf1ccaf9e
KoD <20050904daikikodama@gmail.com>
2020-04-29 20:17:47 +0900

    modify gen_vec

--- template/gen_vec.cpp
+++ template/gen_vec.cpp
@@ -1,10 +1,10 @@
 
 template <class T, std::size_t N, std::size_t I = 0>
-auto gen_vec(const std::size_t (&list)[N], typename std::enable_if<(I == N), const T&>::type value = T()) { 
+decltype(auto) gen_vec(const std::size_t (&list)[N], typename std::enable_if<(I == N), const T&>::type value = T()) { 
   return value; 
 }
 
 template <class T, std::size_t N, std::size_t I = 0>
-auto gen_vec(const std::size_t (&list)[N], typename std::enable_if<(I != N), const T&>::type value = T()) { 
-  return std::vector<decltype(gen_vec<T, N, I + 1>(list, value))>(list[I], gen_vec<T, N, I + 1>(list, value)); 
+decltype(auto) gen_vec(const std::size_t (&list)[N], typename std::enable_if<(I != N), const T&>::type value = T()) { 
+  return std::vector(list[I], gen_vec<T, N, I + 1>(list, value)); 
 }
