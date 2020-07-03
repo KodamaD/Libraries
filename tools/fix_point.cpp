e6cccf40d27a6dd9c31f4a45191fd0b84df15e16
KoD <20050904daikikodama@gmail.com>
2020-07-04 00:07:25 +0900

    delete tools

--- tools/fix_point.cpp
+++
@@ -1,9 +0,0 @@
-
-template <class Func>
-struct fix_point: private Func {
-  explicit constexpr fix_point(Func &&func): Func(std::forward<Func>(func)) { }
-  template <class... Args>
-  constexpr decltype(auto) operator () (Args &&... args) const {
-    return Func::operator()(*this, std::forward<Args>(args)...);
-  }
-};
