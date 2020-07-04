ca6be29cdac22cd0176280a9b163eed7853826da
KoD <20050904daikikodama@gmail.com>
2020-07-04 10:50:36 +0900

    add multi_array

---
+++ other/multi_array.cpp
@@ -0,0 +1,13 @@
+
+template <class T, size_t N, size_t... Seq>
+struct multi_array_impl {
+  using type = std::array<typename multi_array_impl<T, Seq...>::type, N>;
+};
+
+template <class T, size_t N>
+struct multi_array_impl<T, N> {
+  using type = std::array<T, N>;
+};
+
+template <class T, size_t N, size_t... Seq>
+using multi_array = typename multi_array_impl<T, N, Seq...>::type; 
